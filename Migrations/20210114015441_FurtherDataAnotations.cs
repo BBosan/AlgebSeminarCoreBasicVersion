67b2ef99b8c368bd334a35c0d60aa7c914ec87df
BBosna <maracparac@gmail.com>
2021-01-15 01:59:58 +0100

    Migration FurtherDataAno

---
+++ Migrations/20210114015441_FurtherDataAnotations.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SeminarCore2.Migrations
+{
+    public partial class FurtherDataAnotations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Zaposlenik",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Zaposlenik",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Opis",
+                table: "Seminar",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Naziv",
+                table: "Seminar",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Prezime",
+                table: "Predbiljezba",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Ime",
+                table: "Predbiljezba",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Predbiljezba",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BrojTelefona",
+                table: "Predbiljezba",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Adresa",
+                table: "Predbiljezba",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Zaposlenik",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Zaposlenik",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Opis",
+                table: "Seminar",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Naziv",
+                table: "Seminar",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Prezime",
+                table: "Predbiljezba",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Ime",
+                table: "Predbiljezba",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Predbiljezba",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BrojTelefona",
+                table: "Predbiljezba",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Adresa",
+                table: "Predbiljezba",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
