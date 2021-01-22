69956a90bce01c5973a28bfebfeab719d02486e5
BBosna <maracparac@gmail.com>
2021-01-22 01:18:13 +0100

    custom valid email domain attribute

--- Models/RegisterViewModel.cs
+++ Models/RegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SeminarCore2.Controllers;
+using SeminarCore2.Extra;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -13,6 +14,7 @@ namespace SeminarCore2.Models
         [Required]
         [EmailAddress]
         [Remote(action: nameof(AccountController.IsEmailInUse), controller: "Account")]
+        [ValidEmailDomain(allowedDomain: "admin.com", ErrorMessage = "Domain Not Allowed")]
         public string Email { get; set; }
 
         [Required]
