f4b720a6b5d9dcc23d9301d49f2eaabd65f5cbef
BBosna <maracparac@gmail.com>
2021-01-27 00:48:04 +0100

    Add Or Remove Users From Role

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -24,6 +24,7 @@ namespace SeminarCore2.Controllers
             this.roleManager = roleManager;
             this.userManager = userManager;
         }
+        #region CreateRole
 
         [HttpGet]
         public IActionResult CreateRole()
@@ -56,7 +57,8 @@ namespace SeminarCore2.Controllers
             }
 
             return View(model);
-        }
+        } 
+        #endregion
 
         [HttpGet]
         public IActionResult ListRoles() 
@@ -65,12 +67,13 @@ namespace SeminarCore2.Controllers
             return View(roles);
         }
 
+        #region EditRole
         [HttpGet]
         public async Task<IActionResult> EditRole(string id)
         {
             var role = await roleManager.FindByIdAsync(id);
 
-            if (role==null)
+            if (role == null)
             {
                 ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
                 return View("NotFound");
@@ -84,7 +87,8 @@ namespace SeminarCore2.Controllers
 
             foreach (var user in userManager.Users)
             {
-                if (await userManager.IsInRoleAsync(user, role.Name)) {
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
                     model.Users.Add(user.UserName);
                 };
             }
@@ -121,8 +125,92 @@ namespace SeminarCore2.Controllers
                 return View(model);
             }
         }
+        #endregion
 
+        #region EditUsersInRole
+        [HttpGet]
+        public async Task<IActionResult> EditUsersInRole(string roleId)
+        {
+            ViewBag.roleId = roleId;
 
+            var role = await roleManager.FindByIdAsync(roleId);
+            if (role==null)
+            {
+                ViewBag.ErrorMessage = $"Role with id = {roleId} cannot be found";
+                return View("NotFound");
+            }
+
+            var model = new List<UserRoleViewModel>();
+
+            foreach (var user in userManager.Users)
+            {
+                var userRoleViewModel = new UserRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName,
+                    IsSelected = (await userManager.IsInRoleAsync(user, role.Name))/* ? true : false*/
+                };
+
+                #region Longer
+                //if (await userManager.IsInRoleAsync(user, role.Name))
+                //{
+                //    userRoleViewModel.IsSelected = true;     
+                //}
+                //else
+                //{
+                //    userRoleViewModel.IsSelected = false;
+                //} 
+                #endregion
+
+                model.Add(userRoleViewModel);
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUsersInRole(List<UserRoleViewModel> model, string roleId)
+        {
+            var role = await roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {roleId} cannot be found";
+                return View("NotFound");
+            }
+
+            for (int i = 0; i < model.Count; i++)
+            {
+                var user = await userManager.FindByIdAsync(model[i].UserId);
+
+                IdentityResult result = null;
+
+                //if selected and is alrady not in this role
+                if (model[i].IsSelected && !(await userManager.IsInRoleAsync(user, role.Name)))
+                {
+                    result = await userManager.AddToRoleAsync(user, role.Name);
+                }
+                else if (!model[i].IsSelected && await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    result = await userManager.RemoveFromRoleAsync(user, role.Name);
+                }
+                else
+                {
+                    continue;
+                }
+
+                if (result.Succeeded)
+                {
+                    if (i < (model.Count - 1))
+                        continue;
+                    else
+                        return RedirectToAction("EditRole", new { Id = roleId });
+                }
+            }
+
+            return RedirectToAction("EditRole", new { Id = roleId });
+        }
+        #endregion
 
 
 
--- Startup.cs
+++ Startup.cs
@@ -84,15 +84,27 @@ namespace SeminarCore2
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            #region Generated
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //} 
+            #endregion
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseExceptionHandler("/Error");
+                app.UseStatusCodePagesWithReExecute("/Error/{0}"); //placeholder receives 404 or which ever we put as statuscode
             }
 
             app.UseHttpsRedirection();
