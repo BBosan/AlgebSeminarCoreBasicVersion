4eb0e080d4999fb0a02d17eca5c7360109fbd894
BBosna <maracparac@gmail.com>
2021-11-09 17:46:08 +0100

    Filter By Status PredB

--- Controllers/PredbiljezbeController.cs
+++ Controllers/PredbiljezbeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SeminarCore2.Data;
+using SeminarCore2.Extra;
 using SeminarCore2.Models;
 
 namespace SeminarCore2.Controllers
@@ -21,13 +22,123 @@ public class PredbiljezbeController : Controller
         }
 
         // GET: Predbiljezbas
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string sortOrder, string searchString, string currentFilter, int? pageNumber, string currentStatus, string status)
         {
+
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["DatumSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+            ViewData["ImeSortParm"] = String.IsNullOrEmpty(sortOrder) ? "ime_desc" : "";
+            ViewData["PrezimeSortParm"] = String.IsNullOrEmpty(sortOrder) ? "prezime_desc" : "";
+            ViewData["AdresaSortParm"] = String.IsNullOrEmpty(sortOrder) ? "adresa_desc" : "";
+
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+
+            }
+
+            if (status != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                status = currentStatus;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+            ViewData["CurrentStatus"] = status;
+
             var predbiljezbe = _context.Predbiljezbe
                 .Include(p => p.Seminar)
                 .AsNoTracking();
 
-            return View(await predbiljezbe.ToListAsync());
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                predbiljezbe = predbiljezbe.Where(s =>
+                                          s.Ime.Contains(searchString)
+                                       || s.Prezime.Contains(searchString)
+                                       || s.Adresa.Contains(searchString)
+                                       );
+            }
+
+            #region DropDownFilterStatus
+            var options = new SelectListItem[]{
+                new SelectListItem() { Text = "Svi", Value = ""},
+                new SelectListItem() { Text = "Odobreni", Value = "Odobreni" },
+                new SelectListItem() { Text = "Odbijeni", Value = "Odbijeni" },
+                new SelectListItem() { Text = "Neobradjeni", Value = "Neobradjeni" }
+            };
+
+            ViewBag.statusDropDown = new SelectList(options, "Value", "Text", status);
+
+
+            if (!string.IsNullOrEmpty(status))
+            {
+                //predbiljezbe = predbiljezbe.Where(x => x.StatusDaNe == (status == "Odobreni"));
+
+                #region 1
+                //predbiljezbe = predbiljezbe.Where(x => (x.StatusDaNe == false ? "Odbijeni" : !x.StatusDaNe.HasValue ? "Neobradjeni" : "Odobreni").Equals(status)); 
+                #endregion
+
+                #region 2
+                //bool? test = (status == "Neobradjeni") ? (test = null) : (status == "Odobreni");
+                //predbiljezbe = predbiljezbe.Where(x => x.StatusDaNe == test); 
+                #endregion
+
+                #region 3
+
+                predbiljezbe = predbiljezbe.Where(x => 
+                x.StatusDaNe == ((status == "Odobreni") ? true : 
+                    ((status == "Neobradjeni") ?
+                    default(bool?) /*ili (bool?)null*/ 
+                    : 
+                    false))
+                ); 
+
+                #endregion
+
+                #region TestGood
+                //bool? what = (status == "Odobreni") ? true : ((status == "Neobradjeni") ? (bool?)null : false);
+                //string test2 = (what == false ? "Odbijeni" : !what.HasValue ? "Neobradjeni" : "Odobreni"); 
+                #endregion
+
+                #region Nez
+                //b = status?.Equals("Neobradjeni") ?? null; 
+                #endregion
+            }
+
+            #endregion
+
+
+            switch (sortOrder)
+            {
+                case "ime_desc":
+                    predbiljezbe = predbiljezbe.OrderByDescending(s => s.Ime);
+                    break;
+                case "prezime_desc":
+                    predbiljezbe = predbiljezbe.OrderByDescending(s => s.Prezime);
+                    break;
+                case "adresa_desc":
+                    predbiljezbe = predbiljezbe.OrderByDescending(s => s.Adresa);
+                    break;
+                case "Date":
+                    predbiljezbe = predbiljezbe.OrderBy(s => s.Datum);
+                    break;
+                case "date_desc":
+                    predbiljezbe = predbiljezbe.OrderByDescending(s => s.Datum);
+                    break;
+                default:
+                    predbiljezbe = predbiljezbe.OrderBy(s => s.Ime);
+                    break;
+            }
+
+            int pageSize = 50;
+            return View(await PaginatedList<Predbiljezba>.CreateAsync(predbiljezbe.AsNoTracking(), pageNumber ?? 1, pageSize));
         }
 
         // GET: Predbiljezbas/Details/5
